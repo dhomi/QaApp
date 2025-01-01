94e6178979f97768e0f4b0d5e8bf0366c0b7aec2
Beni <gitkraken@dhomi.nl>
2025-01-01 18:05:08 +0100

    benis init commit

---
+++ Controllers/AppQasController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QaApp.Data;
+
+namespace QaApp.Controllers
+{
+    public class AppQasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppQasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AppQas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AppQa.ToListAsync());
+        }
+
+        // GET: AppQas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appQa = await _context.AppQa
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appQa == null)
+            {
+                return NotFound();
+            }
+
+            return View(appQa);
+        }
+
+        // GET: AppQas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppQas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AppVraag,AppAntwoord")] AppQa appQa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(appQa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appQa);
+        }
+
+        // GET: AppQas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appQa = await _context.AppQa.FindAsync(id);
+            if (appQa == null)
+            {
+                return NotFound();
+            }
+            return View(appQa);
+        }
+
+        // POST: AppQas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AppVraag,AppAntwoord")] AppQa appQa)
+        {
+            if (id != appQa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(appQa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppQaExists(appQa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appQa);
+        }
+
+        // GET: AppQas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appQa = await _context.AppQa
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (appQa == null)
+            {
+                return NotFound();
+            }
+
+            return View(appQa);
+        }
+
+        // POST: AppQas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var appQa = await _context.AppQa.FindAsync(id);
+            if (appQa != null)
+            {
+                _context.AppQa.Remove(appQa);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppQaExists(int id)
+        {
+            return _context.AppQa.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QaApp.Data;
+
+namespace QaApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<QaApp.Data.AppQa> AppQa { get; set; } = default!;
+    }
+}
