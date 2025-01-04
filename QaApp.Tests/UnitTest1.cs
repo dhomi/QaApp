0de3e085fb8f4fd8496b0ef89f899ee8f44848ef
Beni <gitkraken@dhomi.nl>
2025-01-04 14:50:24 +0100

    refactor split main and tests

--- QaApp.Tests/UnitTest1.cs
+++ QaApp.Tests/UnitTest1.cs
@@ -1,10 +1,13 @@
-namespace QaApp.Tests;
+using Xunit;
 
-public class UnitTest1
+namespace QaApp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
-
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Test1");
+        }
     }
 }
