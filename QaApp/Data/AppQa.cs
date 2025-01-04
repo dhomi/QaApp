0413207055473dc70352675b014229390c644c6f
Beni <gitkraken@dhomi.nl>
2025-01-04 14:49:35 +0100

    refactor and split main and tests

---
+++ QaApp/Data/AppQa.cs
@@ -0,0 +1,13 @@
+﻿namespace QaApp.Data
+{
+    public class AppQa
+    {
+        public int Id { get; set; }
+        public string? AppVraag { get; set; }
+        public string AppAntwoord { get; set; }
+        public AppQa()
+        {
+            
+        }
+    }
+}
---
+++ QaApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
